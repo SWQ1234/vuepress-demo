c2a1cf9ddaad520c551120a39f82cd431dba5e10
sun <545949068@qq.com>
2021-02-03 01:56:13 +0800

    博客基本搭建完毕

---
+++ docs/.vuepress/components/my-button.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <button @click="handleClick">我是一个按钮</button>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        handleClick() {
+            console.log('点击成功');
+        }
+    },
+}
+</script>
