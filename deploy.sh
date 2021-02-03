a7c6da8f926932de2af40318db46590920dfffa6
sun <545949068@qq.com>
2021-02-03 14:02:42 +0800

    a

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,8 @@ git add -A
 git commit -m 'deploy'
 
 # 如果你想要部署到 https://USERNAME.github.io
-git push -f git@github.com:sunwenqiangy/sunwenqiangy.github.io.git master
+# git@github.com:sunwenqiangy/sunwenqiangy.github.io.git 
+git push -f myblog master
 
 # 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
 # git push -f git@github.com:USERNAME/<REPO>.git master:gh-pages
